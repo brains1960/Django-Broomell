6dd0be2e0b5c845feb44cdfe4b60f4e8a02d0cee
brains1960 <tundunoladipo@gmail.com>
2018-02-02 13:16:52 -0500

    Added Pythonanywhere to Hosts

--- Weather/Weather/settings.py
+++ Weather/Weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nf7jcle5iqm9c*b3eh2e7o3=3eh^2yxorzeg8_h2ocmvb#b%-e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stephenbroomell.pythonanywhere.com',]
 
 
 # Application definition
