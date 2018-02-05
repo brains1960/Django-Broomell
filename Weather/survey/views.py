14da71d1a513e88fdac8f0780652b9df34f1b068
brains1960 <tundunoladipo@gmail.com>
2018-02-04 21:59:46 -0500

    Added Example Page (not accesible)
    cannot get to example from instruction page

--- Weather/survey/views.py
+++ Weather/survey/views.py
@@ -38,11 +38,11 @@ def index(request, participant_id):
 			new_response.week = week
 			new_response.save()
 
-			colour = colours[new_participant.last_trial // 8]
+			colour = colours[participant.last_trial // 8]
 
 			url = '/survey/'+str(participant_id)
 
-			if new_participant.condition == 1:
+			if participant.condition == 1:
 				first_condition_text = "A malfunctioning machine has an identically shaped distribution of internal temperatures as a \
 				 properly function machine, but produces daily temperature readings that are on average 10 degrees Fahrenheit warmer\
 				  than a properly functioning machine."
@@ -51,7 +51,7 @@ def index(request, participant_id):
 				mean temperature of 63 degrees Fahrenheit and a standard deviation of 8 degrees, or if the machine has malfunctioned, \
 				and is running with an average that is 10 degrees warmer than normal."
 
-			elif new_participant.condition == 2:
+			elif participant.condition == 2:
 				first_condition_text = "A malfunctioning machine looks the same as a properly function machine, but produces daily\
 				 temperature readings that are on average 10 degrees Fahrenheit warmer than a properly functioning machine. This \
 				 figure shows 1000 daily temperatures recorded for a malfunctioning injection molding machine."
@@ -105,6 +105,47 @@ def index(request, participant_id):
 
 		form = SurveyForm()
 		# Render the HTML template index.html with the data in the context variable
-		return render(request,'instructions.html', {'form' : form, 'page_name' : participant_id, 'colour' : colour, 
+		return render(request,'instructions.html', {'page_name' : participant_id, 'colour' : colour, 
 						'condition_text_1' : first_condition_text, 'condition_text_2' : second_condition_text})
    
+def example(request, participant_id):
+
+	participant = Participants.objects.get(participant_id=participant_id)
+	global_data = GlobalData.objects.get(participant_id=participant_id)
+
+	if global_data.unit == "C":
+		example1 = "18"
+		example2 = "17"
+		example3 = "16"
+		example4 = "20"
+		example5 = "12"
+		example6 = "21"
+		example7 = "23"
+
+	elif global_data.unit == "F":
+		example1 = "64"
+		example2 = "62"
+		example3 = "61"
+		example4 = "68"
+		example5 = "54"
+		example6 = "69"
+		example7 = "74"
+
+	if (request.method == 'POST'):
+
+		return render(request, 'example.html', {'page_name' : participant_id, 'colour' : colour, 'example1' : example1,
+												'example2' : example2, 'example3' : example3, 'example4' : example4,
+												'example5' : example5, 'example6' : example6, 'example7' : example7})
+
+	else: 
+
+
+		if participant.condition == 1:
+			condition_img = "<img src='../static/img/condition1_example.jpg'>"
+
+		elif participant.condition == 2:
+			condition_img = "<img src='../static/img/condition1_example.jpg'>"
+
+		return render(request, 'example.html', {'page_name' : participant_id, 'colour' : colour, 'example1' : example1,
+												'example2' : example2, 'example3' : example3, 'example4' : example4,
+												'example5' : example5, 'example6' : example6, 'example7' : example7})
